c477d00fce098ec361fff2ded60475911eb1e2f8
Aws_killer <telegramsuper6@gmail.com>
2023-12-03 00:05:28 +0300

    error handling and retrying 3 times

--- App/Chat/utils/PoeBot.py
+++ App/Chat/utils/PoeBot.py
@@ -10,15 +10,27 @@ print(client.get_chat_history()["data"])
 
 
 async def SendMessage(req: BotRequest):
-    global CHAT_CODE
-    if CHAT_CODE == "":
-        for chunk in client.send_message(
-            req.bot, req.message, chatCode="2rx4w5jt6zf96tn7dr1"
-        ):
-            pass
-        CHAT_CODE = chunk["chatCode"]
-    else:
-        for chunk in client.send_message(req.bot, req.message, chatCode=CHAT_CODE):
-            pass
+    global CHAT_CODE, client
+    counter = 0
+    while True:
+        try:
+            if CHAT_CODE == "":
+                for chunk in client.send_message(
+                    req.bot, req.message, chatCode="2rx4w5jt6zf96tn7dr1"
+                ):
+                    pass
+                CHAT_CODE = chunk["chatCode"]
+            else:
+                for chunk in client.send_message(
+                    req.bot, req.message, chatCode=CHAT_CODE
+                ):
+                    pass
 
-    return chunk["text"]
+            return {"response": chunk["text"], "code": 200}
+        except:
+            if counter > 4:
+                return {"response": "Try again later", "code": 500}
+            client = PoeApi("sXvCnfYy8CHnXNTRlxhmVg==")
+            CHAT_CODE = ""
+            counter += 1
+            print(client.get_chat_history()["data"])
